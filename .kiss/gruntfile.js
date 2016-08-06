17e792ac92d37e8df573b1f537492eb651c9ee7c
sixertoy <sixertoy.github@gmail.com>
2016-08-06 14:47:15 +0200

    update gruntfile

--- .kiss/gruntfile.js
+++ .kiss/gruntfile.js
@@ -1,4 +1,4 @@
-/* global module, require */
+/* global module */
 /**
  * <project_name>
  * <project_repo>
@@ -12,6 +12,26 @@ module.exports = function(grunt) {
     'use strict';
 
     // npm i -D grunt load-grunt-config
-    require('load-grunt-config')(grunt);
+    // require('load-grunt-config')(grunt);
+
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['jshint']);
 
 };
